bfe555d0194a4638cad89ef82232a84257966118
lizhimins <707364882@qq.com>
2017-10-12 21:51:05 +0800

    1042

---
+++ lydsy/1042.cpp
@@ -0,0 +1,38 @@
+/**
+»›≥‚‘≠¿Ì 
+**/
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<set>
+#include<ctime>
+#include<queue>
+#include<cmath>
+#include<cstdlib>
+#include<algorithm>
+#define ll long long;
+using namespace std;
+
+int T;
+int c[5],d[5];
+
+inline int read()
+{
+    int x = 0;
+    char ch = getchar();
+    while(ch < '0' || ch > '9') ch = getchar();
+    while(ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x;
+}
+
+int main()
+{
+    T = read();
+    cout << T; 
+    return 0;
+}
+
+
